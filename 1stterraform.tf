0a2c73ea19bc20d9b3a22e426c8d4a5d6678ee62
Trupti <bhandaretrupti07@gmail.com>
2025-06-02 10:57:18 +0530

    backend block terraform

--- 1stterraform.tf
+++ 1stterraform.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket555"
-
+    bucket = "terraform-bucket555"   # ✅ no space here
+    key    = "terraform.tfstate"
     region = "us-east-1"
-    
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
