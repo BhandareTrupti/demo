9937a347e40b1b6928b16d6bb2d5db744ed6da53
Trupti <bhandaretrupti07@gmail.com>
2025-06-02 09:54:44 +0530

    terraform file

--- 1stterraform.tf
+++ 1stterraform.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0953476d60561c955"
   instance_type = "t2.micro"
   key_name = "teraa"
+  vpc_security_group_ids = ["sg-058736fc17770d20f"]
 
   tags = {
     Name = "HelloWorld"
